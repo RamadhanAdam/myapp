a87de6da1781d2091e9fd7667f1797a597fa26da
Your Name <you@example.com>
2025-03-11 18:44:13 +0300

    "props"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Todo from "./components/Todo";
+
 const App = () => {
   return (
     <div>
       <h1> My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text = 'Learn React'/>
+      <Todo text = 'Learn React' />
+      <Todo text = 'Learn React' />
     </div>
   );
 };
