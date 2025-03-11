c7e5ef22600afbbc33426da969ab9f97028aa9fd
Your Name <you@example.com>
2025-03-11 18:41:24 +0300

    "Todo component"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-
-function App() {
-  return <div> Hello!</div>
-}
+import Todo from "./components/Todo";
+const App = () => {
+  return (
+    <div>
+      <h1> My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+};
 
 export default App;
